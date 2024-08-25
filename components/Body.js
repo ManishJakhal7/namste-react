6fb6754a1e1b944327a77b902247c54b9807e581
Manish <jakhalmanish@gmail.com>
2024-08-25 17:35:22 +0530

    button

--- components/Body.js
+++ components/Body.js
@@ -1,13 +1,29 @@
 import RestaurantCard from "./Restaurantcard";
+import { useState } from "../node_modules/react";
 import dataList from "../utilis/mockdata";
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurant] = useState(dataList);
   return (
     <div className="body">
       <div className="search"></div>
-      <div className="restro-container"></div>
-      {dataList.map((restaurant) => (
-        <RestaurantCard resData={restaurant} />
-      ))}
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setListOfRestaurant(filterList);
+          }}
+        >
+          Top Restaurants
+        </button>
+      </div>
+      <div className="restro-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard resData={restaurant} />
+        ))}
+      </div>
     </div>
   );
 };
